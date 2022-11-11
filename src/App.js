d53da0a8f4c7f5f0f366374c18202a6aa25b3849
basemosman2 <49072081+basemosman2@users.noreply.github.com>
2022-11-11 12:26:50 +0000

    added tailwind config to index and nav routes app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import About from './Pages/About';
+import Classes from './Pages/Classes';
+import Exercises from './Pages/Exercises';
+import ContactUs from './Pages/ContactUs';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="">
+      <Navbar/>
       <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/classes" element={<Classes/>}/>
+          <Route path="/exercises" element={<Exercises/>}/>
+          <Route path="/contactUs" element={<ContactUs/>}/>
       </Routes>
     </div>
   );
